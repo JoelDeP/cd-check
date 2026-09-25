9f8d27667a95c78110c32d657eb0d12e483e8966
Joel De Paz <coquidepaz@gmail.com>
2026-09-25 16:41:00 -0400

    Stamp service worker build bac041fc1a53

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * Everything is relative so the app works under a /repo-name/ subpath.
  */
 
-const BUILD = 'f35a34772e79';
+const BUILD = 'bac041fc1a53';
 const SHELL_CACHE = `cd-check-shell-${BUILD}`;
 const IMG_CACHE = 'cd-check-ddragon-img';
 
